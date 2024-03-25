43de8f2c09d6fdc29b72f370f65e2ce343b23812
Ruchitha-kappera <113978060+Ruchitha-kappera@users.noreply.github.com>
2024-03-25 19:51:29 +0530

    Add files via upload

---
+++ assignment1/Odd_position_char.java
@@ -0,0 +1,12 @@
+
+public class OddPositionCharacters {
+    public static void main(String[] args) {
+        String text = "type here to search";
+        
+        for (int i = 0; i < text.length(); i++) {
+            if (i % 2 != 0 && text.charAt(i) != ' ') {
+                System.out.print(text.charAt(i));
+            }
+        }
+    }
+}
